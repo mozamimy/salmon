56aacef0dab9160e2b3ac1bb392f9238cf00ca58
mozamimy <alice@mozami.me>
2019-04-20 18:46:59 +0900

    Test an example of pulldown-cmark
    This code based on
    https://github.com/raphlinus/pulldown-cmark/blob/73e6fa6ebe5ebc14825c4f9f506af89b678bbe97/examples/string-to-string.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate pulldown_cmark;
+
+use pulldown_cmark::{Parser, Options, html};
+
 fn main() {
     println!("Hello, world!");
+
+    let markdown_input: &str = "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+    let mut options = pulldown_cmark::Options::empty();
+    options.insert(pulldown_cmark::Options::ENABLE_STRIKETHROUGH);
+    let parser = pulldown_cmark::Parser::new_ext(markdown_input, options);
+
+    let mut html_output: String = String::with_capacity(markdown_input.len() * 3 / 2);
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+
+    println!("Out: {}", &html_output);
 }
